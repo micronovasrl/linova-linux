3125230f6808b1afb932310efed2cc32101e542d
Giulio Benetti <giulio.benetti@micronovasrl.com>
2018-05-02 17:05:40 +0200

    build.sh: build only specific dts files
    Signed-off-by: Giulio Benetti <giulio.benetti@micronovasrl.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 . ~/env_linova1_setup.sh
-make ARCH=arm zImage dtbs -j8 modules
+make ARCH=arm zImage -j8 modules \
+    sun7i-a20-linova1.dtb \
+    sun7i-a20-linova1-ctp-4_3i.dtb \
+    sun7i-a20-linova1-ctp-7i.dtb
