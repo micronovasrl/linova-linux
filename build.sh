98930ae71a9e08dddc75695e229b5388d99f38de
Giulio Benetti <giulio.benetti@micronovasrl.com>
2018-05-08 17:54:35 +0200

    build.sh: Add -@ to DTC_FLAGS and suppress warnings
    Signed-off-by: Giulio Benetti <giulio.benetti@micronovasrl.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 . ~/env_linova1_setup.sh
-make ARCH=arm zImage -j8 modules \
+make DTC_FLAGS="-@ -W no-unit_address_vs_reg -W no-avoid_unnecessary_addr_size" ARCH=arm zImage -j8 modules \
     sun7i-a20-linova1.dtb \
     sun7i-a20-linova1-ctp-4_3i.dtb \
-    sun7i-a20-linova1-ctp-7i.dtb
+    sun7i-a20-linova1-ctp-7i.dtb \
+    sun7i-a20-linova1-rndis.dtb
